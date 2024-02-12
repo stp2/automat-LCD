7941033a502c8efb5ca97a4a72d72b25ab9be193
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2024-02-12 19:15:22 +0100

    custom chars

--- LCD.ino
+++ LCD.ino
@@ -1,10 +1,30 @@
 #include <LiquidCrystal.h>
 
-#define WHEEL_LEN 10
+#define WHEEL_LEN 8
 #define REELS 3
 
 #define LCD_BACKLIGHT 10
 #define BUTTONS A0
+
+enum fruits {
+    DINO,
+    TREE,
+    HEAD,
+    TWO,
+    CHERRY,
+    APPLE,
+    PEAR,
+    BANANA
+};
+const char dino[8] PROGMEM = {0x06, 0x0D, 0x0F, 0x0E, 0x1F, 0x1E, 0x0A, 0x0A};
+const char tree[8] PROGMEM = {0x04, 0x1F, 0x0E, 0x04, 0x1F, 0x0E, 0x04, 0x04};
+const char head[8] PROGMEM = {0x00, 0x0E, 0x15, 0x1F, 0x0E, 0x00, 0x0E, 0x00};
+const char two[8] PROGMEM = {0x07, 0x01, 0x01, 0x17, 0x04, 0x04, 0x07, 0x00};
+const char cherry[8] PROGMEM = {0x10, 0x08, 0x0C, 0x0B, 0x0B, 0x08, 0x0C, 0x0C};
+const char apple[8] PROGMEM = {0x04, 0x04, 0x0E, 0x1F, 0x1F, 0x1F, 0x0E, 0x00};
+const char pear[8] PROGMEM = {0x00, 0x04, 0x04, 0x0E, 0x0E, 0x1F, 0x1F, 0x0E};
+const char banana[8] PROGMEM = {0x02, 0x04, 0x0C, 0x0C, 0x18, 0x1C, 0x0E, 0x06};
+const char* const fruitsChars[8] PROGMEM = {dino, tree, head, two, cherry, apple, pear, banana};
 
 class automat {
    private:
@@ -93,9 +113,17 @@ class automat {
     }
 };
 
-uint8_t wheel[] = {0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9};
+uint8_t wheel[] = {fruits::DINO, fruits::TREE, fruits::HEAD, fruits::TWO, fruits::CHERRY, fruits::APPLE, fruits::PEAR, fruits::BANANA, fruits::DINO, fruits::TREE};
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 automat mat(lcd, wheel);
+
+void customCharacterLoad(LiquidCrystal& lcd, const char* data, byte addr) {
+    uint8_t buffer[8];
+    for (uint8_t i = 0; i < 8; i++) {
+        buffer[i] = pgm_read_byte(data + i);
+    }
+    lcd.createChar(addr, buffer);
+}
 
 void setup() {
     pinMode(A5, INPUT);
@@ -104,6 +132,11 @@ void setup() {
     pinMode(LCD_BACKLIGHT, OUTPUT);
     digitalWrite(LCD_BACKLIGHT, HIGH);
     lcd.begin(16, 2);
+    // custom characters
+    for (uint8_t i = 0; i < 8; i++) {
+        const char* fruit = (const char*)pgm_read_word(&(fruitsChars[i]));
+        customCharacterLoad(lcd, fruit, i);
+    }
     lcd.home();
     lcd.print("Bet: 10");
     mat.setBet(10);
